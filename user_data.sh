2db59f9ec182d9cf9b8f34fbfa8e7e524dec7fa6
Sumit Tiwari <60305045+sumittiwari022@users.noreply.github.com>
2023-08-26 23:12:59 +0530

    updated queue arm64

--- user_data.sh
+++ user_data.sh
@@ -4,10 +4,10 @@ yum update -y
 #amazon-linux-extras install epel -y
 #yum install erlang -y
 #yum install rabbitmq-server -y
-wget https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.1/erlang-23.2.1-1.el7.x86_64.rpm
-yum localinstall erlang-23.2.1-1.el7.x86_64.rpm -y
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.10.0/rabbitmq-server-3.10.0-1.el8.noarch.rpm
-sudo rpm -Uvh rabbitmq-server-3.10.0-1.el8.noarch.rpm
+wget https://github.com/rabbitmq/erlang-rpm/releases/download/v26.0.2/erlang-26.0.2-1.amzn2023.aarch64.rpm
+yum localinstall erlang-26.0.2-1.amzn2023.aarch64.rpm -y
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.12.4/rabbitmq-server-3.12.4-1.el8.noarch.rpm
+sudo rpm -Uvh rabbitmq-server-3.12.4-1.el8.noarch.rpm
 systemctl enable --now rabbitmq-server.service
 rabbitmq-plugins enable rabbitmq_management
 systemctl enable rabbitmq-server.service
